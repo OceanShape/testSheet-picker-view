5c614ebb7693fe5f5c9e76c60601b2bb67b4b170
OceanShape <oceanshape700@gmail.com>
2022-05-16 20:24:27 +0900

    Refactor: Picker 및 Label UI 코드로 변환

--- picker-view-example/picker-view-example/ViewController.swift
+++ picker-view-example/picker-view-example/ViewController.swift
@@ -1,21 +1,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var pickerView: UIPickerView!
-    
-    @IBOutlet weak var pickedContent: UILabel!
     
     let data: [String] = ["One", "Two", "Three", "Four", "Five"]
+    
+    private let pickerView: UIPickerView = {
+        let view = UIPickerView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBlue
+        return view
+    }()
+    
+    private let pickedContentLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.backgroundColor = .systemRed
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.pickerView.delegate = self
         self.pickerView.dataSource = self
+        
+        layoutPickerView()
+        
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.view.addSubview(pickedContentLabel)
+        
+        NSLayoutConstraint.activate([
+            self.pickedContentLabel.topAnchor.constraint(equalTo: pickerView.bottomAnchor, constant: 50),
+            self.pickedContentLabel.heightAnchor.constraint(equalToConstant: 50),
+            self.pickedContentLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.pickedContentLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
     }
-    
-    
-    
+
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
@@ -33,7 +54,19 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        pickedContent.text = data[row]
+        self.pickedContentLabel.text = data[row]
+    }
+    
+    func layoutPickerView() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.view.addSubview(pickerView)
+        
+        NSLayoutConstraint.activate([
+            self.pickerView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 200),
+            self.pickerView.heightAnchor.constraint(equalToConstant: 200),
+            self.pickerView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.pickerView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
     }
 
 }
